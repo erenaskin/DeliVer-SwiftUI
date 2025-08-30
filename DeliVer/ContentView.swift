361e194518437af557f88fdcc6912297f24593b3
erenaskin <eren.askin@hotmail.com>
2025-08-31 00:03:43 +0300

    Initial Commit

---
+++ DeliVer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeliVer
+//
+//  Created by Eren AŞKIN on 31.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeliVer/DeliVerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliVerApp.swift
+//  DeliVer
+//
+//  Created by Eren AŞKIN on 31.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct DeliVerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
